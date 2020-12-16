6ec7461e44c9f618c2f58effcf44fcd669fa8709
Edward <62472600+horbosis556@users.noreply.github.com>
2020-12-16 21:42:09 +0700

    added problem D

---
+++ Problem D.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Convert .csv to dataframe.
+df = pd.read_csv("C:/Users/cally/Documents/activity.csv")
+
+days_passed = []
+number_of_steps = []
+week-id = []
+week-day = []
+
+df['whatweek'] = pd.to_datetime(df['date']).dtype(dayofweek)
+df["whatweek"] = df["whatweek"].astype(float)
+
+for x in df['date'].unique():
+    days_passed.append(x)
+    number_of_steps.append(df.loc[df['date']==x, 'steps'].sum())
+    week-id.append(df.loc[df['date']==x, 'whatweek'].sum())
+
+for x in week-id:
+    if x == 288.0:
+        week-day.append('weekday')
+    else:
+        week-day.append('weekend')
+
+df = pd.DataFrame({'Days': days_passed, 'Steps': number_of_steps , 'Isweekday': week-day})
+
+print(df)
+
+df.plot(x ='Days', kind = 'line')
+
+plt.title('average number of steps taken')
+plt.xticks(np.linspace(0,61,5))
+plt.ylabel('Steps')
+plt.show()
+
